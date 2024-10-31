77d03ad79efccd86f1cea8e02460865b1017c1a2
yunus kupucu <yunus.kupucu@gmail.com>
2024-11-01 01:08:44 +0300

    create backend and frontend arc

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  // root route http://localhost:5001/
+  res.send("helloooooo!!!");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
